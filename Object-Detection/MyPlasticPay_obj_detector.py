5e64d7e266b4765a356c40c4a8ee2d7faf5985aa
anjanakta <58140600+anjanakta@users.noreply.github.com>
2021-06-18 16:34:21 +0530

    Exits when botte detected

--- Object-Detection/MyPlasticPay_obj_detector.py
+++ Object-Detection/MyPlasticPay_obj_detector.py
@@ -1,10 +1,9 @@
 import cv2
 import numpy as np
-thres = 0.45 # Threshold to detect object
+
+thres = 0.45 
 nms_threshold = 0.2
 cap = cv2.VideoCapture(0)
-cv2.namedWindow("test")
-
 
 classNames= []
 classFile = 'data/coco.names'
@@ -29,12 +28,16 @@ while True:
     confs = list(np.array(confs).reshape(1,-1)[0])
     confs = list(map(float,confs))
     
+    if classIds[[0]] in [44,90]:
+        print("Bottle detected..\nOPENING NOW!!!")
+        break
 
     indices = cv2.dnn.NMSBoxes(bbox,confs,thres,nms_threshold)
  
     for i in indices:
         i = i[0]
         box = bbox[i]
+        
         x,y,w,h = box[0],box[1],box[2],box[3]
         cv2.rectangle(img, (x,y),(x+w,h+y), color=(0, 255, 0), thickness=2)
         cv2.putText(img,classNames[classIds[i][0]-1].upper(),(box[0]+10,box[1]+30),
